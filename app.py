957562e5f61da88c9105de1e2487745a32a7732a
Amanda Botelho de Moraes <32555917+abmoraes16@users.noreply.github.com>
2021-10-19 01:10:49 -0300

    Desafio: Customizar a Mensagem da APP

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Hey... Don't worry, be happy!"
 
 if __name__ == '__main__':
     app.run(debug=True)
